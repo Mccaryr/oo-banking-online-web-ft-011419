32935cac83ae78e9c102c5d9e443c7b111226700
Robert McCary <robert.mccary@yahoo.com>
2019-01-29 19:19:15 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,12 @@
 class BankAccount
+  @@all = []
   attr_reader :name
   attr_accessor :balance, :status, :transfer_amount
   def initialize(name)
     @name=name
     @balance = 1000
     @status= "open"
+    @@all << self 
   end
 
   def deposit(transfer_amount)
