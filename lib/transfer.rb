a307e6fc05f88f21ec6ae2ccc15da315784a0db2
Robert McCary <robert.mccary@yahoo.com>
2019-01-29 19:05:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,9 @@
 class Transfer
   attr_accessor :sender, :receiver, :status
-  def initialize(sender, receiver, transfer_amount)  
-    @sender=sender 
-    @receiver=receiver 
+  def initialize(sender, receiver, transfer_amount)
+    @sender=sender
+    @receiver=receiver
     @status="pending"
-    @transfer_amount = [] 
-  end 
+    @transfer_amount = []
+  end
 end
