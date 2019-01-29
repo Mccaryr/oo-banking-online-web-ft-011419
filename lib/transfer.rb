1c1fab76b3520b6b440b32ce92501ececd829254
Robert McCary <robert.mccary@yahoo.com>
2019-01-29 19:59:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,17 +16,17 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.balance < amount 
+    if sender.balance < amount
       status = "rejected"
       return "Transaction rejected, you broke dawg"
-    elsif 
+    elsif
       status == "complete"
       puts "Transaction has already been executed"
-    else 
-      sender.deposit -= amount 
-      receiver.deposit += amount 
+    else
+      sender.deposit -= amount
+      receiver.deposit += amount
       status = "complete"
-    end 
-  end 
-      
+    end
+  end
+
 end
